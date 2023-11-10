96abee7345b668d7eeb32faabbbadaee3e47f1ec
Rippel, Matthias (SE-A/35) <matthias.rippel@volkswagen.de>
2023-11-10 12:53:21 +0100

    added createProduct() Endpoint to ProductController

--- src/main/java/de/vs/product/ProductController.java
+++ src/main/java/de/vs/product/ProductController.java
@@ -1,5 +1,21 @@
 package de.vs.product;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+	
+	@Autowired
+	ProductRepository productRepository;
+	
+	@PostMapping
+	public Product createProduct() {
+		Product product = new Product();
+		return productRepository.save(product);
+	}
 
 }
